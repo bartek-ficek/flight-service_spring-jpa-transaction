57d688fb02959b669c51b97c4b13dda26826c24e
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-21 16:10:29 +0100

    initial commit

---
+++ src/test/java/pl/bartekficek/tx/FlightServiceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bartekficek.tx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightServiceExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
