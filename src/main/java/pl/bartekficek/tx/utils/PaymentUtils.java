7f55248e09d1d9e5746c978c315599450dcdcf8f
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-21 19:14:22 +0100

    created PaymentUtils class & method validateCreditLimit() (NOT COMPLETED)

--- src/main/java/pl/bartekficek/tx/utils/PaymentUtils.java
+++ src/main/java/pl/bartekficek/tx/utils/PaymentUtils.java
@@ -8,6 +8,16 @@ public class PaymentUtils {
     private static Map<String, Double> paymentMap = new HashMap<>();
 
     {
-        paymentMap.put()
+        paymentMap.put("account1",12000.0);
+        paymentMap.put("account2",10000.0);
+        paymentMap.put("account3",5000.0);
+        paymentMap.put("account4",8000.0);
+    }
+
+    public static boolean validateCreditLimit(String accountNo, double paidAmount) {
+        if (paidAmount>paymentMap.get(accountNo)) {
+
+        } else
+            return true;
     }
 }
