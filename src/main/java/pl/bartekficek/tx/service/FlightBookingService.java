2ff92b1ba871ac2500051f14212e489b9e0d9e05
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-21 19:50:49 +0100

    completed method bookFlightTicket() in FlightBookService

--- src/main/java/pl/bartekficek/tx/service/FlightBookingService.java
+++ src/main/java/pl/bartekficek/tx/service/FlightBookingService.java
@@ -8,6 +8,9 @@ import pl.bartekficek.tx.entity.PassengerInfo;
 import pl.bartekficek.tx.entity.PaymentInfo;
 import pl.bartekficek.tx.repository.PassengerInfoRepository;
 import pl.bartekficek.tx.repository.PaymentInfoRepository;
+import pl.bartekficek.tx.utils.PaymentUtils;
+
+import java.util.UUID;
 
 @Service
 public class FlightBookingService {
@@ -22,9 +25,15 @@ public class FlightBookingService {
         FlightBookingAcknowledgement acknowledgement = null;
 
         PassengerInfo passengerInfo = request.getPassengerInfo();
-        passengerInfoRepository.save(passengerInfo);
+        passengerInfo = passengerInfoRepository.save(passengerInfo);
 
         PaymentInfo paymentInfo = request.getPaymentInfo();
-    }
 
+        PaymentUtils.validateCreditLimit(paymentInfo.getAccountNo(),passengerInfo.getFare());
+
+        paymentInfo.setPassengerId(passengerInfo.getPId());
+        paymentInfo.setAmount(passengerInfo.getFare());
+        paymentInfoRepository.save(paymentInfo);
+        return new FlightBookingAcknowledgement("SUCCESS", passengerInfo.getFare(), UUID.randomUUID().toString().split("-")[0],passengerInfo);
+    }
 }
--- src/main/java/pl/bartekficek/tx/utils/PaymentUtils.java
+++ src/main/java/pl/bartekficek/tx/utils/PaymentUtils.java
@@ -1,5 +1,8 @@
 package pl.bartekficek.tx.utils;
 
+import org.hibernate.InstantiationException;
+import pl.bartekficek.tx.exception.InsufficientAmountException;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,15 +11,15 @@ public class PaymentUtils {
     private static Map<String, Double> paymentMap = new HashMap<>();
 
     {
-        paymentMap.put("account1",12000.0);
-        paymentMap.put("account2",10000.0);
-        paymentMap.put("account3",5000.0);
-        paymentMap.put("account4",8000.0);
+        paymentMap.put("account1", 12000.0);
+        paymentMap.put("account2", 10000.0);
+        paymentMap.put("account3", 5000.0);
+        paymentMap.put("account4", 8000.0);
     }
 
     public static boolean validateCreditLimit(String accountNo, double paidAmount) {
-        if (paidAmount>paymentMap.get(accountNo)) {
-
+        if (paidAmount > paymentMap.get(accountNo)) {
+            throw new InsufficientAmountException("Insufficient fund!");
         } else
             return true;
     }
