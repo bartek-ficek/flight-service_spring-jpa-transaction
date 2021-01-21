c10a41c43ba564e3bc7557fae319431ab573112d
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-21 19:57:55 +0100

    created endpoint 'bookFlightTicket'

--- src/main/java/pl/bartekficek/tx/FlightServiceExampleApplication.java
+++ src/main/java/pl/bartekficek/tx/FlightServiceExampleApplication.java
@@ -1,10 +1,26 @@
 package pl.bartekficek.tx;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pl.bartekficek.tx.dto.FlightBookingAcknowledgement;
+import pl.bartekficek.tx.dto.FlightBookingRequest;
+import pl.bartekficek.tx.service.FlightBookingService;
 
 @SpringBootApplication
+@RestController
 public class FlightServiceExampleApplication {
+
+    @Autowired
+    private FlightBookingService service;
+
+    @PostMapping("/bookFlightTicket")
+    public FlightBookingAcknowledgement bookFlightTicket(@RequestBody FlightBookingRequest request) {
+        return service.bookFlightTicket(request);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FlightServiceExampleApplication.class, args);
