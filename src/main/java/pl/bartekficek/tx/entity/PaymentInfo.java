3d982c07f3ce84fc842cc3d2931cc5e86233026b
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-21 18:19:39 +0100

    Entity PaymentInfo created

--- src/main/java/pl/bartekficek/tx/entity/PaymentInfo.java
+++ src/main/java/pl/bartekficek/tx/entity/PaymentInfo.java
@@ -1,4 +1,28 @@
 package pl.bartekficek.tx.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="PAYMENT_INFO")
 public class PaymentInfo {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name="uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    private String paymentId;
+    private String accountNo;
+    private double amount;
+    private String CardType;
+    private Long passengerId;
 }
