42685a572db71414803145353374638b008f4ef5
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-21 19:17:52 +0100

    created InsufficientAmountException

---
+++ src/main/java/pl/bartekficek/tx/exception/InsufficientAmountException.java
@@ -0,0 +1,8 @@
+package pl.bartekficek.tx.exception;
+
+public class InsufficientAmountException extends RuntimeException {
+
+    public InsufficientAmountException (String msg) {
+        super(msg);
+    }
+}
